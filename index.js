27a997db6fbc947c1f626934619c8f808631a598
Thiagosnts <39925677+Thiagosnts@users.noreply.github.com>
2021-07-05 16:38:42 -0300

    alter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/config/custom-express.js');
 
 
-app.listen(3003,()=> console.log("Porta 3002 ouvindo !!!"));
+app.listen(3000,()=> console.log("Porta 3000 ouvindo !!!"));
 
